65aa7a4d94416a6dbdaf94f60cc0f796b2cd9147
Johnvique <johnvique@gmail.com>
2019-07-11 14:21:31 +0300

    innitial commit

---
+++ mtaanitech.py
@@ -0,0 +1,31 @@
+price =20
+print  (price)
+
+name =input ('what is your name? ')
+favorite_color =input ('what is your favorite color? ')
+print(name + ' likes ' + favorite_color)
+
+birth_year =input ('Birth year: ')
+print(type(birth_year))
+age = 2019 - int(birth_year)
+print(type(age))
+print(age)
+
+weight_lbs =input('weight (lbs): ')
+weight_kg =int(weight_lbs) * 0.45
+print(weight_kg)
+
+course = input("python's course for beginner ")
+print(course)
+
+course = '''
+Thank you people for making me understand the fundamentals of python programming
+
+
+I really appreciate your continous support each time
+May God bless you for your kind help
+Thank you
+
+'''
+
+print(course[0])
